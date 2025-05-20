2e6f718c9fdab72bfa889a97767060d97ab849df
kamto-s <kamto.076s@gmail.com>
2025-05-20 16:22:48 +0700

    new file:   .gitattributes 	new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/kamto/chat/chat_app/ChatAppApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/kamto/chat/chat_app/ChatAppApplicationTests.java

---
+++ src/test/java/com/kamto/chat/chat_app/ChatAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kamto.chat.chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//
+	}
+
+}
